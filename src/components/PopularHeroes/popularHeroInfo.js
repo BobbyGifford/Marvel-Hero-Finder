b4e399f5f992423ad8606d2a1d3316585c480026
juice <Bobby>
2018-05-21 11:30:55 -0700

    Exported popular data and made profile images clickable.

---
+++ src/components/PopularHeroes/popularHeroInfo.js
@@ -0,0 +1,29 @@
+import thorImg from "../../images/thor2.jpg"
+import ironmanImg from "../../images/ironman2.jpg"
+import captainamericaImg from "../../images/captainamerica2.jpg"
+import hulkImg from "../../images/hulk2.jpg"
+
+const popularHeroes = [
+    {
+        title: "Thor",
+        description: "Thor's description",
+        image: thorImg
+    },
+    {
+        title: "Iron Man",
+        description: "Iron Man's description",
+        image: ironmanImg
+    },
+    {
+        title: "Captain America",
+        description: "Captain America's description",
+        image: captainamericaImg
+    },
+    {
+        title: "Hulk",
+        description: "Hulk's description",
+        image: hulkImg
+    },
+];
+
+export default popularHeroes;
--- src/components/PopularHeroes/popularHeroes.js
+++ src/components/PopularHeroes/popularHeroes.js
@@ -1,33 +1,7 @@
 import React from 'react'
-import thorImg from "../../images/thor_1.jpg"
-import ironmanImg from "../../images/ironman_1.jpg"
-import captainamericaImg from "../../images/captainamerica_1.jpg"
-import hulkImg from "../../images/hulk_1.jpg"
+import popularHeroes from './popularHeroInfo'
 import {Link} from 'react-router-dom'
 import "./popularHeroes.css"
-
-const popularHeroes = [
-    {
-        title: "Thor",
-        description: "Thor's description",
-        image: thorImg
-    },
-    {
-        title: "Iron Man",
-        description: "Iron Man's description",
-        image: ironmanImg
-    },
-    {
-        title: "Captain America",
-        description: "Captain America's description",
-        image: captainamericaImg
-    },
-    {
-        title: "Hulk",
-        description: "Hulk's description",
-        image: hulkImg
-    },
-];
 
 const mapOverHeroes = () => {
     return popularHeroes.map(hero => {
@@ -35,11 +9,11 @@ const mapOverHeroes = () => {
             <div className="col s12 m3" key={hero.title}>
                 <div className="card">
                     <div className="card-image">
-                        <img className="popular-card" src={hero.image} alt={hero.title}/>
+                        <Link to={`/profile/${hero.title}`}><img className="popular-card" src={hero.image} alt={hero.title}/></Link>
                     </div>
                     <div className="card-content">
                         <span className="card-title">{hero.title}</span>
-                        <p>{hero.description}</p>
+                        {/*<p>{hero.description}</p>*/}
                     </div>
                     <div className="card-action">
                     <Link className="waves-effect waves-light red darken-3 btn white-text"
@@ -55,6 +29,7 @@ const mapOverHeroes = () => {
 const PopularHeroes = () => {
     return (
         <div className="row center-align">
+            <h2>Popular Heroes</h2>
             {mapOverHeroes()}
         </div>
     )
